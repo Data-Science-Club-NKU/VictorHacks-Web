359a9d76ad3d7d11bd49f73a7ae6189396fe5e87
ak-codes-py <khanal.aditya1122@gmail.com>
2025-02-08 16:45:21 -0500

    mentors component redone.

--- victorhacks-web/components/mentor.tsx
+++ victorhacks-web/components/mentor.tsx
@@ -2,11 +2,19 @@ import { PulsatingButton } from "./ui/pulsating-button";
 
 export default function Mentor() {
   return (
-    <div className="flex flex-col items-center justify-center h-96 bg-gray-900 text-white p-8 m-12 rounded-lg shadow-lg bg-cover bg-center" style={{ backgroundImage: "url('https://news.microsoft.com/wp-content/uploads/prod/sites/93/2017/02/Girls-in-STEM-gif-1.gif')"}}>
-      <div className="text-5xl font-bold h-[10vh] flex items-end justify-end">
-        <h1>Become a mentor!</h1>
+    <div
+      className="relative h-96 bg-gray-900 text-white p-8 m-12 rounded-lg shadow-lg bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url('https://news.microsoft.com/wp-content/uploads/prod/sites/93/2017/02/Girls-in-STEM-gif-1.gif')",
+      }}
+    >
+      <div className="absolute top-4 left-4 flex flex-col">
+        <h1 className="text-5xl font-bold mb-4 font-Tomorrow font-medium">Become a mentor!</h1>
+        <PulsatingButton className="w-fit px-6 py-2 rounded-full mx-auto">
+          <a href="https://forms.gle/R7HMizRmsPzsc3VZ8">Apply Now</a>
+        </PulsatingButton>
       </div>
-      <PulsatingButton className="mt-6"><a href="https://forms.gle/R7HMizRmsPzsc3VZ8">Apply Now</a></PulsatingButton>
     </div>
   );
 }
