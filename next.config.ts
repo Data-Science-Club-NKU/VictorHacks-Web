15485c0a85737660e2c952b613aaa4e845618e3a
ak-codes-py <khanal.aditya1122@gmail.com>
2025-02-25 11:23:17 -0500

    Fix Next.js export configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: "export", // 
+  images: {
+    unoptimized: true, // ✅ Fix image errors when using GitHub Pages
+  },
+  trailingSlash: true, // ✅ Helps GitHub Pages find static routes correctly
 };
 
 module.exports = nextConfig;
